49b65c6f8b79cfded60fe4cca21f2ff287262446
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-11-17 17:17:16 -0200

    Sing up with Foursquare working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:foursquare]
 
-  def self.from_omniauth(auth)
-    binding.pry
+  def self.find_for_foursquare_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      binding.pry
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.email.first_name
+      user.last_name = auth.info.email.last_name
     end
   end
 end

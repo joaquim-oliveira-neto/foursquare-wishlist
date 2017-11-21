227a8d1e490273f948947be377ec23acc6484ade
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2017-11-21 17:28:55 -0200

    Service implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,35 +1,9 @@
 class PagesController < ApplicationController
 
   def home
-    create_client
-
-    @checkins = []
-    @client.recent_checkins.each do |checkin|
-      venue = checkin.venue
-      user  = checkin.user
-
-      next if Place.find_by(checkin_id: checkin.id)
-      next if user.id == current_user.uid
-
-      @checkins << {
-        checkin_id:  checkin.id,
-        venue_photo: get_venue_photo_as_url(venue),
-        venue_name:  venue.name,
-        user_photo:  get_user_photo_as_url(user)
-      }
-    end
-
-    @wishlist = []
-    user_places = current_user.places
-    user_places.each do |place|
-      checkin = @client.checkin(place.checkin_id)
-      venue = checkin.venue
-      @wishlist << {
-        place_id: place.id,
-        venue_photo: get_venue_photo_as_url(venue),
-        venue_name:  venue.name,
-        }
-    end
+    fs = FoursquareService.new(user: current_user)
+    @checkins = fs.get_recent_checkins_formatted
+    @wishlist = fs.get_whishlist_formatted
   end
 
   def create
@@ -45,36 +19,6 @@ class PagesController < ApplicationController
   def destroy
     Place.find(params[:id]).destroy
     redirect_to root_path
-  end
-
-  private
-
-  def create_client
-    @client = Foursquare2::Client.new(
-      :client_id => ENV['FOURSQUARE_ID'],
-      :client_secret => ENV['FOURSQUARE_SECRET'],
-      :api_version => '20171117',
-      :oauth_token => current_user.access_token
-    )
-  end
-
-
-  def get_venue_photo_as_url(venue)
-    venue_photos = @client.venue_photos(venue.id)
-    return "" if venue_photos.items.empty?
-
-    venue_photo_params = venue_photos.items[0]
-    photo_prefix       = venue_photo_params.prefix
-    photo_suffix       = venue_photo_params.suffix
-
-    "#{photo_prefix}300x300#{photo_suffix}"
-  end
-
-  def get_user_photo_as_url(user)
-    photo_prefix       = user.photo.prefix
-    photo_suffix       = user.photo.suffix
-
-    "#{photo_prefix}100x100#{photo_suffix}"
   end
 
 end
--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,37 +1,69 @@
 class FoursquareService
   def initialize(params = {})
     @user = params[:user]
-    create_client
+    @client = create_client
   end
 
   def get_recent_checkins_formatted
+    checkins = []
     @client.recent_checkins.each do |checkin|
       venue = checkin.venue
       user  = checkin.user
 
+      next if Place.find_by(checkin_id: checkin.id)
+      next if user.id == @user.uid
+
       checkins << {
         checkin_id:  checkin.id,
-        venue_photo: "",#get_venue_photo_as_url(venue),
+        venue_photo: get_venue_photo_as_url(venue),
         venue_name:  venue.name,
-        user_photo:  "",#get_user_photo_as_url(user)
+        user_photo:  get_user_photo_as_url(user)
       }
     end
-
-    return checkins
+    checkins
   end
 
-  def get_wishlist_formatted
-
+  def get_whishlist_formatted
+    wishlist = []
+    user_places = @user.places
+    user_places.each do |place|
+      checkin = @client.checkin(place.checkin_id)
+      venue = checkin.venue
+      wishlist << {
+        place_id: place.id,
+        venue_photo: get_venue_photo_as_url(venue),
+        venue_name:  venue.name,
+        }
+    end
+    wishlist
   end
 
   private
 
   def create_client
-    @client = Foursquare2::Client.new(
+    Foursquare2::Client.new(
       :client_id => ENV['FOURSQUARE_ID'],
       :client_secret => ENV['FOURSQUARE_SECRET'],
       :api_version => '20171117',
       :oauth_token => @user.access_token
     )
+  end
+
+  def get_venue_photo_as_url(venue)
+    venue_photos = @client.venue_photos(venue.id)
+    return "" if venue_photos.items.empty?
+
+    venue_photo_params = venue_photos.items[0]
+    photo_prefix       = venue_photo_params.prefix
+    photo_suffix       = venue_photo_params.suffix
+
+    "#{photo_prefix}300x300#{photo_suffix}"
+  end
+
+  def get_user_photo_as_url(user)
+    photo_prefix       = user.photo.prefix
+    photo_suffix       = user.photo.suffix
+
+    "#{photo_prefix}100x100#{photo_suffix}"
   end
 end
